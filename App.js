8fd8655ee5371d9d38264e44e6b0c0bc7e20810e
sojung32 <sojung0320@gmail.com>
2025-10-12 14:29:32 +0900

    ~16

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{margin: 16, borderWidth: 1, borderColor: 'red', padding: 16}}>Hello World!</Text>
+      <Text style={styles.dummyText}>Hello World2!</Text>
+      <Button title='Tap me!'/>
     </View>
   );
 }
@@ -17,4 +17,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText: {
+    margin: 16, 
+    borderWidth: 1, 
+    borderColor: 'red', 
+    padding: 16
+  }
 });
